0dbc268592153dce4a0ffd8e236dde4e540b34cd
moez-rd <moez.muhardin@gmail.com>
2022-01-20 22:44:50 +0700

    Optimize scrollbar

--- src/components/EntryMain.vue
+++ src/components/EntryMain.vue
@@ -6,7 +6,7 @@
       overflow-y-auto
       h-screen
       scroll-smooth
-      scrollbar-track-gray-200 dark:scrollbar-track-gray-900 scrollbar-thumb-primary-300 dark:scrollbar-thumb-gray-700 scrollbar-thin
+      scrollbar-track-gray-200 dark:scrollbar-track-gray-900 scrollbar-thumb-primary-300 dark:scrollbar-thumb-gray-700 lg:scrollbar-thin
       bg-gray-200
       dark:bg-gray-900
     "
