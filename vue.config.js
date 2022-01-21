e6eee976de2cb4a9e7982d0deeeb17ec9ad19dea
moez-rd <moez.muhardin@gmail.com>
2022-01-21 14:29:36 +0700

    Change favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'KBBI Daring',
+    },
+  },
 })
